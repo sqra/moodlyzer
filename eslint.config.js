4af12541d7ced141ed295b5620f9f67fd300f583
Paweł Skórka <skorkapawel@gmail.com>
2025-03-04 12:46:05 +0100

    Initial commit

--- eslint.config.js
+++ eslint.config.js
@@ -2,32 +2,34 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
 
-export default tseslint.config(
-  {
-    extends: [
-      js.configs.recommended,
-      'plugin:react/recommended',
-      'plugin:prettier/recommended',
-      'plugin:react-hooks/recommended',
-      'plugin:react-refresh/recommended',
-      ...tseslint.configs.recommended,
-    ],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      prettier: 'eslint-plugin-prettier',
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      'prettier/prettier': 'error',
-    },
-  }
-);
+export default {
+  extends: [
+    js.configs.recommended,
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react-refresh/recommended',
+    'prettier', 
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2020: true,
+  },
+  plugins: [
+    'react-hooks',
+    'react-refresh',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    ...reactHooks.configs.recommended.rules,
+  },
+};
