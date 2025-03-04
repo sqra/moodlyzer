78668cebcec320619a675c9ceea17ade0aa96c11
Paweł Skórka <skorkapawel@gmail.com>
2025-03-04 12:47:17 +0100

    Initial commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,5 @@
 import js from '@eslint/js';
-import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import prettier from 'eslint-plugin-prettier';
 
 export default {
   extends: [
